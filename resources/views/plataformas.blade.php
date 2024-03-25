d1a1bdda8d7df625b3fd5755b5c3a990a069910c
Ivanegas <paola.martinez2907@gmail.com>
2024-03-25 11:38:27 -0600

    :construction: Add desing for platforms

--- resources/views/plataformas.blade.php
+++ resources/views/plataformas.blade.php
@@ -5,23 +5,54 @@
 <br>
 <br>
 <div class="container">
-          @foreach ($plataformas as $plataforma)
-<div class="col-xxl-4 col-md-6">
-    <div class="card info-card revenue-card">
+    <section class="section dashboard">
+        <div class="row">
 
-        <div class="card-body">
-            <h5 class="card-title">{{ $plataforma->nombre }}</h5>
-
-            <div class="d-flex align-items-center">
-                <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-currency-dollar"></i>
-                </div>
-                <div class="ps-3">
-                    <h6>{{ $plataforma->perfiles_disponibles }}</h6>
-                </div>
+        <!-- Left side columns -->
+        <div class="col-lg-20">
+            <div class="row">
+            @foreach ($plataformas as $plataforma)
+                <!-- Platform Card -->
+                <div class="col-xxl-4 col-md-6">
+                    <div class="card info-card customers-card">
+                        
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-camera-reels-fill" style="color: #004260"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h5 class="card-title">Plataforma: {{ $plataforma->nombre }}</h5>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-people"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>Perfiles Disponibles: {{ $plataforma->perfiles_disponibles }}</h6>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-person-dash"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>Perfiles Ocupados: {{ $plataforma->perfiles_ocupados }}</h6>
+                                    </div>
+                                </div>
+                                <br>
+                                <button type="button" class="btn btn-primary">
+                                    <i class="ri-account-circle-line"></i>Ver Perfiles
+                                </button>
+                            </div>
+                    </div>
+                </div><!-- End Platform Card-->
+            @endforeach
             </div>
         </div>
-
-    </div>
+        </div>
+</section>
 </div>
-@endforeach
