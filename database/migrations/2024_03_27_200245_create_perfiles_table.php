18488a4752b59e44baf1442fb72a0f36cde5b93b
Ivanegas <paola.martinez2907@gmail.com>
2024-04-02 11:53:52 -0600

    :sparkles: add profiles, and account tables

---
+++ database/migrations/2024_03_27_200245_create_perfiles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('perfiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cuenta_id')->constrained();
+            $table->string('nombre');
+            $table->string('contraseña');
+            $table->date('fecha_inicio');
+            $table->integer('pin_usuario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('perfiles');
+    }
+};
